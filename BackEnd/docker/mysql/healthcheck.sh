0905fbfdcb338a92d030d221b6effed9271461ac
JungMeeni <klk9043@dgu.ac.kr>
2026-05-28 21:42:53 +0900

    merge

--- BackEnd/docker/mysql/healthcheck.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mysqladmin ping -h localhost -uroot -p"$MYSQL_ROOT_PASSWORD" > /dev/null 2>&1
--- BackEnd/fastapi-server/app/routers/route.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.post("/calculate")
-async def calculate_route(data: dict):
-    # 가중치 계산 로직
-    return {"message": "ok"}
